231fb1dd244f14578d76d6d538b7a19a51e9b6ff
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2025-03-01 18:12:24 +0000

    Update Executives.tsx

--- src/components/home/Executives.tsx
+++ src/components/home/Executives.tsx
@@ -6,7 +6,7 @@ const executives = [
   {
     name: 'Dr Hayford NoMore Walls',
     role: 'Chief Executive Officer',
-    image: '',
+    image: './assets/images/nomorewalls.jpg',
     linkedin: '#',
     facebook: '#',
     email: 'nomorewalls@gmail.com',
